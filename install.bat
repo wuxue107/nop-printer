0c0e314e66cbbdb57a161ae136b0fec612d63f8c
nop <575065955@qq.com>
2021-02-03 23:27:18 +0800

    add php7.3 alone

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ cd %SCRIPT_PATH%
 .\archive\unzip .\archive\php7.3.4nts.zip -d .\archive\
 
 cd ..
-set PHP_PATH=%SCRIPT_PATH%\archive\php7.3.4nts
+set PHP_PATH=%SCRIPT_PATH%archive\php7.3.4nts
 set PATH=%PHP_PATH%;%PATH%
 
-php -m
+php -i
