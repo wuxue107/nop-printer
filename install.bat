e924974e6fdb3c7bda7f37f169bdad4e879ad8fe
nop <575065955@qq.com>
2021-02-04 02:50:32 +0800

    add vendor

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ cd %SCRIPT_PATH%
 
 echo/
 echo #######################################################
-echo ############  解压缩 php7.3 独立 安装包   ##############
+echo ############  unzip php7.3 独立 安装包   ##############
 echo #######################################################
 .\archive\unzip -o .\archive\php7.3.4nts.zip -d .\archive\
 set PHP_PATH=%SCRIPT_PATH%archive\php7.3.4nts
@@ -15,12 +15,12 @@ set PATH=%PHP_PATH%;%PATH%
 copy /Y .env.example .env
 echo/
 echo #######################################################
-echo ############  启动 printer api service   ##############
+echo ############  start printer api service   ##############
 echo #######################################################
 start printer-start.bat
 
 echo/
 echo #######################################################
-echo ############  打开 printer setting web page ###########
+echo ############  open printer setting web page ###########
 echo #######################################################
 printer-setting.bat
