e7a860d1bb7d71095881abb3e60f971165347617
nop <575065955@qq.com>
2021-02-23 15:40:28 +0800

    截图

--- bin/test.js
+++ bin/test.js
@@ -6,4 +6,5 @@ var command = helper.argsParser(system.args.slice(1));
 console.log(command.getOption("name","default"));
 console.log(command.getOption("sleep-time",100));
 console.log(command.getOption("isDefault",true));
+console.log(command.getOption("noExist"));
 
