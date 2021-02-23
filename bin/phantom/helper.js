860a6368d7d70e42376f30f17d3a2c2908d78301
nop <575065955@qq.com>
2021-02-23 15:04:52 +0800

    截图

--- bin/phantom/helper.js
+++ bin/phantom/helper.js
@@ -4,14 +4,58 @@
 
 "use strict";
 
-exports.start = function () {
-    console.log('Starting the universe....');
-}
+/**
+ * 去两边空格
+ * @return {string}
+ */
+var trim  = function() {
+    return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+};
 
+/**
+ * 转驼峰
+ * @param str
+ * @return {string}
+ */
+var toCamel = function(str) {
+    return str.replace(/([^_\s\-])(?:[_\s-]+([^_\s-]))/g, function ($0, $1, $2) {
+        return $1 + $2.toUpperCase();
+    });
+};
 
 /**
  * 解析命令行参数
  */
-exports.argsParser = function(args){
-    
+var argsParser = function(optionArgs){
+    var options = {
+    };
+
+    var index = 1;
+    for(var i in optionArgs){
+        var optionArg = optionArgs[i];
+        var matchs = optionArg.match(/^--([\w-]+)(=(.*))?$/);
+        if(!matchs){
+            options[index] = optionArg;
+            index++;
+        }else{
+            var optionName = toCamel(matchs[1]);
+            options[optionName] = matchs[3]
+        }
+    }
+
+    options.getOption = function(name,defaultValue){
+        var name = toCamel(name);
+        if(options[name] === undefined){
+            return defaultValue;
+        }
+        
+        return options[name];
+    };
+
+    return options;
 };
+
+
+exports.trim = trim;
+exports.toCamel = toCamel;
+exports.argsParser = argsParser;
