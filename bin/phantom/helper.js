11fccffb273d2ab4f9ac4dd7b171373d8eec5213
nop <575065955@qq.com>
2021-02-23 18:02:32 +0800

    截图

--- bin/phantom/helper.js
+++ bin/phantom/helper.js
@@ -113,7 +113,7 @@ var capturePageElement = function(userOption){
         }
     };
     
-    option = defaultOption.assign(userOption);
+    option = Object.assign(defaultOption,userOption);
 
     exitPage = function (msg) {
         console.info('PAGE EXIT: ' + msg)
@@ -197,93 +197,13 @@ var capturePageElement = function(userOption){
     }
 };
 
-var isPlainObject = function( obj ) {
-    // Not plain objects:
-    // - Any object or value whose internal [[Class]] property is not "[object Object]"
-    // - DOM nodes
-    // - window
-    if ( typeof obj !== "object" || obj.nodeType || isArray(obj)) {
-        return false;
-    }
 
-    if ( obj.constructor ) {
-        return false;
-    }
-
-    // If the function hasn't returned already, we're confident that
-    // |obj| is a plain object, created by {} or constructed with new Object
-    return true;
-};
 
 var isArray = Array.isArray;
 var isFunction = function (obj) {
     return typeof obj === 'function';
 };
 
-var extend = function() {
-    var options, name, src, copy, copyIsArray, clone,
-        target = arguments[0] || {},
-        i = 1,
-        length = arguments.length,
-        deep = false;
-
-    // Handle a deep copy situation
-    if ( typeof target === "boolean" ) {
-        deep = target;
-
-        // Skip the boolean and the target
-        target = arguments[ i ] || {};
-        i++;
-    }
-
-    // Handle case when target is a string or something (possible in deep copy)
-    if ( typeof target !== "object" && !isFunction(target) ) {
-        target = {};
-    }
-
-    // Extend jQuery itself if only one argument is passed
-    if ( i === length ) {
-        target = this;
-        i--;
-    }
-
-    for ( ; i < length; i++ ) {
-        // Only deal with non-null/undefined values
-        if ( (options = arguments[ i ]) != null ) {
-            // Extend the base object
-            for ( name in options ) {
-                src = target[ name ];
-                copy = options[ name ];
-
-                // Prevent never-ending loop
-                if ( target === copy ) {
-                    continue;
-                }
-
-                // Recurse if we're merging plain objects or arrays
-                if ( deep && copy && ( isPlainObject(copy) || (copyIsArray = isArray(copy)) ) ) {
-                    if ( copyIsArray ) {
-                        copyIsArray = false;
-                        clone = src && isArray(src) ? src : [];
-
-                    } else {
-                        clone = src && isPlainObject(src) ? src : {};
-                    }
-
-                    // Never move original objects, clone them
-                    target[ name ] = extend( deep, clone, copy );
-
-                    // Don't bring in undefined values
-                } else if ( copy !== undefined ) {
-                    target[ name ] = copy;
-                }
-            }
-        }
-    }
-
-    // Return the modified object
-    return target;
-};
 
 exports.trim = trim;
 exports.toCamel = toCamel;
@@ -292,6 +212,5 @@ exports.capturePageElement = capturePageElement;
 exports.captureElement = captureElement;
 exports.isArray = isArray;
 exports.isFunction = isFunction;
-exports.isPlainObject = isPlainObject;
-exports.extend = extend;
+
 
